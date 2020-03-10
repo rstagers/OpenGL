480aa9885c16e1e04a02cd9faea557544f5e735b
Randy Stagers <kf7ekb@gmail.com>
2020-03-10 13:36:39 -0700

    Fixed GLCore/WindowsWindow.cpp  OpenGL Hints added to prevent Segmentation Fault.  Added a new project and documented how to add a new project to the make system using premake5.  First try is TheCherno batch rendering lessons.

--- OpenGL-Core/src/Platform/Windows/WindowsWindow.cpp
+++ OpenGL-Core/src/Platform/Windows/WindowsWindow.cpp
@@ -45,7 +45,11 @@ namespace GLCore {
 			glfwSetErrorCallback(GLFWErrorCallback);
 			s_GLFWInitialized = true;
 		}
-
+	    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 
 		glfwMakeContextCurrent(m_Window);
--- premake5.lua
+++ premake5.lua
@@ -70,3 +70,49 @@ group ""
 
 includeexternal "OpenGL-Core"
 include "OpenGL-Examples"
+
+-- This stuff is added to create a new sanddbox!
+-- Copy the OpenGL-Sandbox directory to a new
+-- directory with the project name.  Then
+-- below to end of file copy and add a new section
+-- cange the workspace names (see comment)
+-- then run scripts/Linux-Premake.sh
+-- The Makefile will be created in the new project
+-- root directory.
+
+-- OpenGL-FirstTry
+workspace "OpenGL-FirstTry"			-- Change
+    startproject "OpenGL-FirstTry"	-- Change
+    architecture "x64"
+    startproject "OpenGL-FirstTry"	-- Change
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+    flags
+    {
+        "MultiProcessorCompile"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to OpenGL-Core
+IncludeDir = {}
+IncludeDir["GLFW"] = "vendor/glfw/include"
+IncludeDir["Glad"] = "vendor/Glad/include"
+IncludeDir["ImGui"] = "vendor/imgui"
+IncludeDir["glm"] = "vendor/glm"
+IncludeDir["stb_image"] = "vendor/stb_image"
+
+-- Projects
+group "Dependencies"
+    includeexternal "OpenGL-Core/vendor/glfw"
+    includeexternal "OpenGL-Core/vendor/Glad"
+    includeexternal "OpenGL-Core/vendor/imgui"
+group ""
+
+includeexternal "OpenGL-Core"
+include "OpenGL-FirstTry"  -- Change
