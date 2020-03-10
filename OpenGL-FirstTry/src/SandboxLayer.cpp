63bb1f99f6674e3cdd0d2851c62e77f04a0056be
Randy Stagers <kf7ekb@gmail.com>
2020-03-10 15:03:50 -0700

    Introduction to Batch rendering.

--- OpenGL-FirstTry/src/SandboxLayer.cpp
+++ OpenGL-FirstTry/src/SandboxLayer.cpp
@@ -29,10 +29,15 @@ void SandboxLayer::OnAttach()
 	glBindVertexArray(m_QuadVA);
 
 	float vertices[] = {
+		-1.5f, -0.5f, 0.0f,
 		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
+		-0.5f,  0.5f, 0.0f,
+		-1.5f,  0.5f, 0.0f,
+		
+         0.5f, -0.5f, 0.0f,
+		 1.5f, -0.5f, 0.0f,
+		 1.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f
 	};
 
 	glCreateBuffers(1, &m_QuadVB);
@@ -42,7 +47,8 @@ void SandboxLayer::OnAttach()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
 
-	uint32_t indices[] = { 0, 1, 2, 2, 3, 0 };
+	uint32_t indices[] = { 0, 1, 2, 2, 3, 0,
+                           4, 5, 6, 6, 7, 4 };
 	glCreateBuffers(1, &m_QuadIB);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
@@ -73,6 +79,11 @@ void SandboxLayer::OnEvent(Event& event)
 			return false;
 		});
 }
+static void SetUniformMat4(uint32_t shader, const char* name, const glm::mat4& matrix)
+{
+    int loc = glGetUniformLocation(shader, name);
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(matrix));
+}
 
 void SandboxLayer::OnUpdate(Timestep ts)
 {
@@ -83,21 +94,23 @@ void SandboxLayer::OnUpdate(Timestep ts)
 
 	glUseProgram(m_Shader->GetRendererID());
 
-	int location = glGetUniformLocation(m_Shader->GetRendererID(), "u_ViewProjection");
+    int location = glGetUniformLocation(m_Shader->GetRendererID(), "u_ViewProjection");
 	glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(m_CameraController.GetCamera().GetViewProjectionMatrix()));
 
 	location = glGetUniformLocation(m_Shader->GetRendererID(), "u_Color");
 	glUniform4fv(location, 1, glm::value_ptr(m_SquareColor));
 
 	glBindVertexArray(m_QuadVA);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+	glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
 }
 
 void SandboxLayer::OnImGuiRender()
 {
-	ImGui::Begin("Controls");
+	/*
+    ImGui::Begin("Controls");
 	if (ImGui::ColorEdit4("Square Base Color", glm::value_ptr(m_SquareBaseColor)))
 		m_SquareColor = m_SquareBaseColor;
 	ImGui::ColorEdit4("Square Alternate Color", glm::value_ptr(m_SquareAlternateColor));
 	ImGui::End();
+    */
 }
